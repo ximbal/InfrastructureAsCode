fc964373accdf762be7c86b8d975a3373bc381d4
ximbal <mail@mail.com>
2017-08-04 13:45:31 -0400

    Update Install.sh @ Multiple Box Flavors

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-set -x
+while getopts flavor: option
+do
+	case "${option}"
+	in
+	flavor) FLAVOR=${OPTARG};;
+    esac
+done
 
-TERRAFORM_VERSION="0.9.5"
-PACKER_VERSION="0.10.2"
-# create new ssh key
-[[ ! -f /home/ubuntu/.ssh/mykey ]] \
-&& mkdir -p /home/ubuntu/.ssh \
-&& ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
-&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+if FLAVOR="1"
+	username="ubuntu"
 
-# install packages
+	# install packages
 apt-get update
-apt-get -y install docker.io ansible unzip
-# add docker privileges
-usermod -G docker ubuntu
-# install pip
+apt-get -y install ansible unzip
+	# install docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce
+
+	# add docker privileges
+usermod -G docker $username
+	# install pip
 pip install -U pip && pip3 install -U pip
 if [[ $? == 127 ]]; then
     wget -q https://bootstrap.pypa.io/get-pip.py
@@ -45,3 +62,55 @@ P_RETVAL=$?
 
 # clean up
 apt-get clean
+
+else if FLAVOR="2"
+	username="centos"
+yum update
+# install packages
+yum install ansible unzip
+# install docker
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum install docker-ce
+
+	# add docker privileges
+usermod -G docker $username
+systemctl start docker
+# install pip
+pip install -U pip && pip3 install -U pip
+if [[ $? == 127 ]]; then
+    wget -q https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py
+    python3 get-pip.py
+fi
+# install awscli and ebcli
+pip install -U awscli
+pip install -U awsebcli
+
+#terraform
+T_VERSION=$(terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+T_RETVAL=${PIPESTATUS[0]}
+
+[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# packer
+P_VERSION=$(packer -v)
+P_RETVAL=$?
+
+[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
+&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+# clean up
+yum clean all
+
+
+
+fi
